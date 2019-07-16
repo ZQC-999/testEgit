54a880a1b190cd4497bbcb1f42fb14d6174c35fe
qichang666 <993311410@qq.com>
2019-07-16 19:01:37 +0800

    解决冲突！

--- src/com/atguigu/egit/test/RgitTest.java
+++ src/com/atguigu/egit/test/RgitTest.java
@@ -4,10 +4,7 @@ public class RgitTest {
 	public static void main(String[] args) {
 		System.out.println("aaaaa");
 		System.out.println("AAAAA");
-<<<<<<< HEAD
 		System.out.println("bbbbb");
-=======
 		System.out.println("BBBBB");
->>>>>>> branch 'master' of https://github.com/ZQC-999/testEgit.git
 	}
 }
